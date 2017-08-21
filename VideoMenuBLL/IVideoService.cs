6bc6ff09f8f7136ba3aabcebb10c56304f49427a
CrystalOfLife <nicolai7@godmail.dk>
2017-08-21 10:46:31 +0200

    moved to 3 layer achitecture

---
+++ VideoMenuBLL/IVideoService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VideoMenuEntity;
+
+namespace VideoMenuBLL
+{
+    public interface IVideoService
+    {
+        Video Create(Video video);
+
+        List<Video> GetAll();
+        Video get(int Id);
+
+        Video Update(Video video);
+
+        Video Delete(int Id);
+    }
+}
---
+++ VideoMenuBLL/Services/VideoService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VideoMenuDAL;
+using VideoMenuEntity;
+
+namespace VideoMenuBLL.Services
+{
+    class VideoService : IVideoService
+    {
+        public Video Create(Video video)
+        {
+            Video newVideo;
+            FakeDB.videos.Add(newVideo = new Video()
+            {
+                Name = video.Name,
+                Id = FakeDB.Id++
+            });
+            return newVideo;
+        }
+
+        public Video Delete(int Id)
+        {
+            foreach (var video in FakeDB.videos)
+            {
+                if (video.Id == Id)
+                {
+                    FakeDB.videos.Remove(video);
+                    return video;
+                }
+            }
+            return null;
+        }
+
+        public Video get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Video> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Video Update(Video video)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
